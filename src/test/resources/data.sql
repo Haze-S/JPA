5f001ecacc009799af53adf94459d9c6da2bc2d6
Haze-S <skdml20022@gmail.com>
2022-01-29 03:06:19 +0900

    N 1 이슈

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -15,3 +15,13 @@ insert into book(`id`, `name`, `publisher_id`, `deleted`, `status`) values (1, '
 insert into book(`id`, `name`, `publisher_id`, `deleted`, `status`) values (2, 'Spring', 1, false, 200);
 
 insert into book(`id`, `name`, `publisher_id`, `deleted`, `status`) values (3, 'SpringBoot', 1, true, 100);
+
+insert into review(`id`, `title`, `content`, `score`, `user_id`, `book_id`) values (1, '내 인생을 바꾼 책', '너무 좋아요', 5.0, 1, 1);
+
+insert into review(`id`, `title`, `content`, `score`, `user_id`, `book_id`) values (2, '재미 없어요', '별로에요', 3.0, 2, 2);
+
+insert into comment(`id`, `comment`, `review_id`) values (1, '저도 재밌었어요', 1);
+
+insert into comment(`id`, `comment`, `review_id`) values (2, '별로....', 1);
+
+insert into comment(`id`, `comment`, `review_id`) values (3, '저도 그냥 그랬어요.', 2);
